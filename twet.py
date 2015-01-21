580c965e5c2cb380783d71fcadc765433a4e8fae
anandhakumar <anandhakumar.radhakrishnan@mercuryminds.com>
2015-01-21 14:46:51 +0530

    hi

--- twet.py
+++ twet.py
@@ -9,7 +9,7 @@ consumer_key = ''
 consumer_secret = ''
 access_token = '2874668814-'
 access_token_secret = ''
- 
+
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
