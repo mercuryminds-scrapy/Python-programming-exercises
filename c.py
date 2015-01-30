ace910b88d9e95095850329d51eb1e7ca06d1a19
anandhakumar <anandhakumar.radhakrishnan@mercuryminds.com>
2015-01-30 17:46:27 +0530

    hi

--- c.py
+++ c.py
@@ -1,1 +1,48 @@
 __author__ = 'anandhakumar'
+
+
+def fib():
+    a=0
+    b=1
+    print a,b,
+    for i in range(20):
+        c=a+b
+        a=b
+        b=c
+        print c,
+
+
+fib()
+
+num=6
+
+for i in range(2,num):
+    if (num % i) == 0:
+        print(num,"is not a prime number")
+        print(i,"times",num//i,"is",num)
+        break
+else:
+    print "on"
+
+
+num=7
+c=1
+for i in range(1,num+1):
+    c*=i
+print c
+
+x=range(30)
+print x
+print x[::-1]
+
+x=['annad','aaaaaadddddd','ffffff','rrllll']
+print x
+x.reverse()
+print x
+print x[::-1]
+print [x[i][::-1] for i in range(len(x))]
+
+
+# bfs
+# linked list
+# tree
